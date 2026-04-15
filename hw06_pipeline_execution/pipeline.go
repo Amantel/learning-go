cecf00b0b6cf5adfafda37531ec5cdeebebb422f
Amantel <amantels@gmail.com>
2026-04-15 21:37:03 +0300

    hw06

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,33 @@ type (
 
 type Stage func(in In) (out Out)
 
+func orDone(done In, in In) Out {
+	out := make(chan interface{})
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- v:
+				case <-done:
+					return
+				}
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, stage := range stages {
+		out = stage(orDone(done, out))
+	}
+	return out
 }
